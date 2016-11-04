a1c3896a8ed04c90f41da88abbfbf314a5c485be
rickie120243 <rickie120243@gmail.com>
2016-11-04 12:20:19 +0800

    test new function docToXml

--- test.js
+++ test.js
@@ -7,8 +7,7 @@ var JSZip = require('jszip');
 describe('convert', function () {
   it('convert check', function () {
     var correct = '༄༅། །དཀར་མཛེས་ཨ་ཡིག་ལས་འཁྲུངས་ཤེས་བློའི་གཏེར། །ཕས་རྒོལ་ཝ་སྐྱེས་ཟིལ་གནོན་གདོང་ལྔ་བཞིན། །ཆགས་ཐོགས་ཀུན་བྲལ་མཚུངས་མེད་འཇམ་དབྱངས་མཐུས། །མ་ཧཱ་མཁས་པའི་གཙོ་བོ་ཉིད་གྱུར་ཅིག །ཅེས་པ་འདི་ཉིད་ཤིན་ཏུ་གྲགས་པའི་དབྱངས་ཚིག་གོ །';
-    var zip = new JSZip(data);
-    var xml = zip.file("word/document.xml").asText();
+    var xml = parser.docxToXml(data);
     var doc = parser.docxToJson(xml);
     parser.toUnicode(doc);
     data = parser.jsonToHtml(doc);
